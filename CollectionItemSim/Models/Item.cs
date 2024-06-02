24f646d0b8dc9d78d30d8b78538d67cca2555745
Ryan Milem <gint271@gmail.com>
2024-06-01 17:15:13 -0700

    Add project files.

---
+++ CollectionItemSim/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionItemSim.Models
+{
+
+    // 10 -> 11 needs 1000 xp
+
+    internal class Item
+    {
+        public int level;
+        public int xp;
+    }
+}
